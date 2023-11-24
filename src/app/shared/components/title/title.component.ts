290c08ff6a78cbee7f6cd19fcbaf36e7c21b9b0a
Jerónimo Gascón Sánchez <jerogassan@gmail.com>
2023-11-24 14:59:18 +0100

    Add title component with HTML, SCSS, spec, and TypeScript files

---
+++ src/app/shared/components/title/title.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-title',
+  standalone: true,
+  imports: [CommonModule, RouterLink],
+  templateUrl: './title.component.html',
+  styleUrl: './title.component.scss',
+})
+export class TitleComponent {
+  img = {
+    link: '/',
+    src: '/assets/imgs/feather.png',
+    alt: 'Logotipo editorial de la escritora española Rosa Celiberti Ribera',
+  };
+
+  title = {
+    name: 'Rosa',
+    surname: 'Celiberti',
+    lastname: 'Ribera',
+  };
+}
