a1eb8353b2042bd901725f73793e3240de3b76d4
Jerónimo Gascón Sánchez <jerogassan@gmail.com>
2023-11-24 14:58:41 +0100

    Add socials component with HTML, SCSS, spec, and TypeScript files

---
+++ src/app/shared/components/socials/socials.component.ts
@@ -0,0 +1,30 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
+import { FacebookComponent } from './icons/facebook/facebook.component';
+import { InstagramComponent } from './icons/instagram/instagram.component';
+import { TiktokComponent } from './icons/tiktok/tiktok.component';
+import { XComponent } from './icons/x/x.component';
+
+@Component({
+  selector: 'app-socials',
+  standalone: true,
+  imports: [
+    CommonModule,
+    RouterLink,
+    FacebookComponent,
+    InstagramComponent,
+    XComponent,
+    TiktokComponent,
+  ],
+  templateUrl: './socials.component.html',
+  styleUrl: './socials.component.scss',
+})
+export class SocialsComponent {
+  socialLinks = {
+    facebook: '#',
+    instagram: '#',
+    x: '#',
+    tiktok: '#',
+  };
+}
