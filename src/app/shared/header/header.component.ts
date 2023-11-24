7e37233578e9793cc127e6eb9b22010f97ca00dc
Jerónimo Gascón Sánchez <jerogassan@gmail.com>
2023-11-24 14:59:33 +0100

    Add header component

---
+++ src/app/shared/header/header.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SocialsComponent } from '../components/socials/socials.component';
+import { TitleComponent } from '../components/title/title.component';
+import { NavbarComponent } from '../components/navbar/navbar.component';
+
+@Component({
+  selector: 'app-header',
+  standalone: true,
+  imports: [CommonModule, SocialsComponent, TitleComponent, NavbarComponent],
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.scss',
+})
+export class HeaderComponent {}
